1ef416a26718f92892108237211d13443a002539
J0NGS <jnetogoncalo@gmail.com>
2023-05-19 18:04:56 -0300

    1.3_starte

--- mars/tools/EscalonamentoPreemptivo.java
+++ mars/tools/EscalonamentoPreemptivo.java
@@ -1,8 +1,21 @@
 package mars.tools;
 import javax.swing.*;
+
+import javafx.event.ActionEvent;
+import mars.mips.SO.ProcessManager.TabelaProcessos;
+import mars.mips.SO.ProcessManager.Escalonador;
+
 import java.awt.*;
 import java.awt.event.*;
 import mars.*;
+import java.util.Observable;
+import mars.util.SystemIO;
+
+
+
+import mars.mips.hardware.AccessNotice;
+import mars.mips.hardware.Memory;
+import mars.mips.hardware.MemoryAccessNotice;
 
 
 /*
@@ -32,6 +45,7 @@ public class EscalonamentoPreemptivo extends AbstractMarsToolAndApplication {
 
    private static String heading =  "Escalonamento preemptivo";
    private static String version = " Version 1.0";
+   public static boolean podeExecutar = true;
 
     public EscalonamentoPreemptivo(String title, String heading) {
       super(title,heading);
@@ -44,7 +58,7 @@ public class EscalonamentoPreemptivo extends AbstractMarsToolAndApplication {
 
 
     public static void main(String[] args) {
-      new IntroToTools(heading+", "+version,heading).go();
+      new EscalonamentoPreemptivo(heading+", "+version,heading).go();
    }
 
     // serve para setar o nome da tool
@@ -52,35 +66,142 @@ public class EscalonamentoPreemptivo extends AbstractMarsToolAndApplication {
       return "Escalonamento Preemptivo";
    }
 
-    protected JComponent buildMainDisplayArea() {
-      Box boxArea = Box.createHorizontalBox();
+  protected int ultimoEndereco = -1; // comparativo de endereço
+  protected int counter = 0;
+	private JTextField counterField; // campo para contador
 
-      JTextField textField = new JTextField();
-      boxArea.add(textField);
+	protected int countTimer = 10; // Timer de interrupção
+	
+	protected int countInter = 0; // contador de interrupções
+	private JTextField counterInterField; // campo para o countInter
+   
+  protected int countInst = 3;
+	private JProgressBar progressbarInst;
 
-      ActionListener taskPerformer = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent evt) {
-            textField.setText("Atualizou");
-        }
-      };
 
-      JButton startButton = new JButton("Iniciar");
-      startButton.addActionListener(
-          new ActionListener() {
-            @Override
-              public void actionPerformed(ActionEvent e) {
-                String textFieldValue = textField.getText();
-                textField.setEditable(false);
-                Timer timer = new Timer(Integer.parseInt(textFieldValue), taskPerformer);
-                timer.start();
-              }
-      });
+	private JToggleButton timerOn; // ligar o timer
+	private JSpinner timerConfig;	
 
-      startButton.addKeyListener(new EnterKeyListener(startButton));
-      boxArea.add(startButton);
+  @Override
+	protected JComponent buildMainDisplayArea() {
+		JPanel panel = new JPanel(new GridBagLayout());
 
-      return new JScrollPane(boxArea);
-   }
+		counterField = new JTextField("0", 10);
+		counterField.setEditable(false);
 
+		counterInterField = new JTextField("0", 10);
+		counterInterField.setEditable(false);
+
+		progressbarInst = new JProgressBar(JProgressBar.HORIZONTAL);
+		progressbarInst.setStringPainted(true);
+
+		timerOn = new JToggleButton("ON/OFF"); 
+		timerOn.setToolTipText("Enable interruption");
+
+		timerConfig = new JSpinner();
+		timerConfig.setModel(new SpinnerNumberModel(10, 2, 100, 1));
+		timerConfig.setToolTipText("Sets the time for the interruption");
+
+		// Add them to the panel
+
+		// Fields
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.LINE_START;
+		c.gridheight = c.gridwidth = 1;
+		c.gridx = 3;
+		c.gridy = 1;
+		c.insets = new Insets(0, 0, 17, 0);
+		panel.add(counterField, c);
+
+		c.insets = new Insets(0, 0, 0, 0);
+		c.gridy++;
+		panel.add(counterInterField, c);
+
+		// progress bar
+		c.gridy++;
+		panel.add(progressbarInst,c);
+		// spinner
+		c.gridy++;
+		panel.add(timerConfig, c);
+
+		// Labels
+		c.anchor = GridBagConstraints.LINE_END;
+		c.gridx = 1;
+		c.gridwidth = 2;
+		c.gridy = 1;
+		c.insets = new Insets(0, 0, 17, 0);
+		panel.add(new JLabel("Instructions so far: "), c);
+
+		c.insets = new Insets(0, 0, 0, 0);
+		c.gridx = 2;
+		c.gridwidth = 1;
+		c.gridy++;
+		panel.add(new JLabel("Interruptions so far: "), c);
+		c.gridy++;
+		panel.add(new JLabel("Time so far: "), c);
+		c.gridy++;
+		panel.add(new JLabel("Timer: "), c);
+
+		// lock
+		c.insets = new Insets(3, 3, 3, 3);
+		c.gridx = 4;
+		c.gridy = 2;
+		panel.add(timerOn, c);
+
+   		return panel;
+	}
+
+//	@Override
+	protected void addAsObserver() {
+		addAsObserver(Memory.textBaseAddress, Memory.textLimitAddress);
+	}
+
+//  @Override
+	protected void processMIPSUpdate(Observable memory, AccessNotice notice) {
+		podeExecutar = true;
+		if (notice.getAccessType() != AccessNotice.READ) return;
+		MemoryAccessNotice m = (MemoryAccessNotice) notice;
+		int a = m.getAddress();
+		if (a == ultimoEndereco) return;
+		if (TabelaProcessos.getProcExect() != null) {
+			ultimoEndereco = a;
+			++counter;
+			if(timerOn.isSelected()) {
+				++countInst;
+				if (countInst > (int)timerConfig.getValue()) {
+					podeExecutar = false;
+					SystemIO.printString("-- Hora de trocar!\n");
+					++countInter; // incrementa interrupções
+					countInst = 0; // zera o progressBar				
+				//ProcessesTable.setProcessoExecutando("roteamento");  // aqui era processChange
+        Escalonador.escalonar(); // vai ser esse?
+				}
+			}
+			updateDisplay();
+		}
+	}
+
+//  @Override
+	protected void initializePreGUI() {
+		countInst = 0;
+		countTimer = 10;
+		countInter = 0;
+		ultimoEndereco = -1;
+	}
+
+//  @Override
+	protected void reset() {
+		countInst = 0;
+		countTimer = 10;
+		countInter = 0;
+		ultimoEndereco = -1;		
+		updateDisplay();
+	}
+//  @Override
+	protected void updateDisplay() {
+		counterField.setText(String.valueOf(counter));
+		counterInterField.setText(String.valueOf(countInter));
+		progressbarInst.setValue(countInst);
+		progressbarInst.setMaximum((int)timerConfig.getValue());
+	}
 }
